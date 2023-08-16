36741ffd6130519c3a243ed64422c0c3047fc85e
798ankita <ankitasharmawins@gmail.com>
2023-08-16 17:01:42 +0530

    connection with mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017',{dbName:'employeedb'}),],
   controllers: [AppController],
   providers: [AppService],
 })
