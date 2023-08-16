621e6b41f6506c6dd2411b15c9f733f1b23cec52
798ankita <ankitasharmawins@gmail.com>
2023-08-16 18:48:56 +0530

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017',{dbName:'employeedb'}),],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Employee'),EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
