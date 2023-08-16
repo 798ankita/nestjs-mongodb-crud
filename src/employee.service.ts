ba89cd526a1edd4f955d99b19f0fb03ecd716a39
798ankita <ankitasharmawins@gmail.com>
2023-08-16 18:50:28 +0530

    employee services

--- src/employee.service.ts
+++ src/employee.service.ts
@@ -10,24 +10,29 @@ import {Employee,EmployeeDocument} from './schema/employee.schema';
   
     constructor(@InjectModel(Employee.name) private readonly employeeModel: Model < EmployeeDocument > ) {}
   
+    //Adding employee using create()
     async create(createEmployeeDto: CreateEmployeeDto): Promise < EmployeeDocument > {
       const employee = new this.employeeModel(createEmployeeDto);
       return employee.save();
     }
-  
+
+  //Fetching employee (s) findAll().
     async findAll(): Promise < EmployeeDocument[] > {
       return this.employeeModel.find()
         .exec();
     }
   
+    // Fetching employee findOne.
     async findOne(id: string) {
       return this.employeeModel.findById(id);
     }
   
+    // Updating an employee using update()
     async update(id: string, updateEmployeeDto: UpdateEmployeeDto): Promise < EmployeeDocument > {
       return this.employeeModel.findByIdAndUpdate(id, updateEmployeeDto);
     }
   
+    // Deleting an employee using remove()
     async remove(id: string) {
       return this.employeeModel.findByIdAndRemove(id);
     }
